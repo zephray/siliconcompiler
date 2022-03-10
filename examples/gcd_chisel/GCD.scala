6f207315f04a27d2f2c5df725e35917736f49f41
Noah Moroze <noah@zeroasic.com>
2022-03-10 18:53:40 -0500

    Move all frontend tutorials into examples

--- examples/gcd_chisel/GCD.scala
+++ examples/gcd_chisel/GCD.scala
@@ -5,7 +5,7 @@ import chisel3._
   * Subtracts the smaller from the larger until register y is zero.
   * value in register x is then the GCD
   */
-class gcd extends Module {
+class GCD extends Module {
   val io = IO(new Bundle {
     val value1        = Input(UInt(16.W))
     val value2        = Input(UInt(16.W))
